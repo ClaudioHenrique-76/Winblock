7c71d1b20958fcfd8d4180dcd461599b9c55ced6
CLAUDIO HENRIQUE SANTOS COSTA <itachiuchiha1.me@gmail.com>
2026-02-10 20:45:19 -0300

    Add files via upload

---
+++ K1/E3/E3/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    //Entrada
+    float Preco, Unidade, Reajuste;
+    float total a pagar, total a pagar com descont, desconto;
+    int quantidade;
+    printf("Mercado:");
+    printf("Dados da compra:");
+    printf("Preco do produto: %f");
+    scanf("%f", Preco);
+    printf("Quantidade: %i");
+    scanf("%i", Unidade);
+    printf("Desconto: %i");
+    scanf("%i", Reajuste);
+
+    //processamento
+    TAP = Preco *
+    return 0;
+}
